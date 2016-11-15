29b62c304fd4c18e78cebc10ec603a5f9b7b33c2
Fredy A Gomez <fredygomez1981@gmail.com>
2016-11-14 20:38:05 -0800

    Update ReadingAttributesD3V4.js

--- src/js/modules/ReadingAttributesD3V4.js
+++ src/js/modules/ReadingAttributesD3V4.js
@@ -6,7 +6,7 @@ import Layer from 'grommet/components/Layer';
 import Heading from 'grommet/components/Heading';
 import * as d3 from 'd3';
 
-class ReadingAttributesD3V4 extends Component {
+export default class ReadingAttributesD3V4 extends Component {
   constructor () {
     super();
     this._OnClick = this._OnClick.bind(this);
@@ -36,32 +36,27 @@ class ReadingAttributesD3V4 extends Component {
     let w = this.props.y;
     let p = this.props.x + 176;
     let entity = this.props.entity;
-    var attributes_map = this.props.comment.attributes.map (function(comment) {
+    let attributes_map = this.props.comment.attributes.map ((comment) => {
       y = y+18;
       return (
-	   <text x={x+5} y={y}>{comment.name}</text>
+	<text x={x+5} y={y}>{comment.name}</text>
       );
     });
-    var relationships_map = this.props.comment.relationship.map (function(commentI) {
-      var to = commentI.to;
-      var new_x;
-      var new_y;	  
-      var search_entity_map = this.props.search_entity.map (function(commentII) {
+    let relationships_map = this.props.comment.relationship.map ((commentI) => {
+      let to = commentI.to;
+      let new_x;
+      let new_y;	  
+      let search_entity_map = this.props.search_entity.map ((commentII) => {
         if (to == commentII.name) {
           new_x = commentII.xpos;
           new_y = commentII.ypos;
           return ({new_x: commentII.xpos,new_y:commentII.ypos});
         }
-        
-      }, this);
-      console.log(search_entity_map);
-	  
+      });  
       if (new_x && new_y) {
         if (new_x > x) {
           return (
-		  <line x1={new_x} y1={new_y+40} x2={z + 176} y2={w+40} stroke="black"></line>
-		  /*<path d={"M"+ new_x +" "+(new_y+40)+"l-20 0 l0"+" "+(w+40)+"L"+(z + 176)+" "+(w+40)} stroke="black" fill="transparent" />  */
-		  
+	 <line x1={new_x} y1={new_y+40} x2={z + 176} y2={w+40} stroke="black"></line>	  
           );
         } else if (new_x == x && new_y > y) {
           return (
@@ -82,32 +77,52 @@ class ReadingAttributesD3V4 extends Component {
         }
       }
       return true;
-    }, this);
-	
+    });
     let rightTable;
     if(this.state.flag_rightTable) {
-      rightTable = (
-	  
-		<Layer closer={true} align="right" onClose={this._onClose}>
-	  
-		<Heading tag="h2">
-		  {entity}
-		</Heading>
-       <hr/><Table><thead><tr><th><b>Owner</b></th><th><b>Policy</b></th><th><b>Request</b></th></tr></thead><tbody><tr><td>Matt McDonald</td><td>Policy XXXX</td><td>Request XXX YYY ZZZ</td></tr><tr><td>Robert Ross</td><td>Policy YYYY</td><td>Request XXX YYY ZZZ</td></tr><tr><td>Matt McDonald</td><td>Policy ZZZZZ</td><td>Request XXX YYY ZZZ</td></tr></tbody></Table>
-	</Layer>
-
+      rightTable = (  
+        <Layer closer={true} align="right" onClose={this._onClose}>
+	  <Heading tag="h2">
+	    {entity}
+	  </Heading>
+          <hr/>
+	  <Table>
+	    <thead>
+	      <tr>
+	        <th><b>Owner</b></th>
+	        <th><b>Policy</b></th>
+	        <th><b>Request</b></th>
+	      </tr>
+	    </thead>
+	    <tbody>
+	      <tr>
+	        <td>XXXX</td>
+	        <td>Policy XXXX</td>
+	        <td>Request XXX YYY ZZZ</td>
+	      </tr>
+	      <tr>
+	        <td>YYYYY</td>
+	        <td>Policy YYYY</td>
+	        <td>Request XXX YYY ZZZ</td>
+	      </tr>
+	      <tr>
+	        <td>ZZZZZZZ</td>
+	        <td>Policy ZZZZZ</td>
+	        <td>Request XXX YYY ZZZ</td>
+	      </tr>
+	    </tbody></Table>
+	  </Layer>
       );
     }	
     return (
-<g>
-
-  <rect x={z} y={w} rx="6" ry="6" width="176" height="130.76923076923077" fill="#BFDFBF" stroke="black" style={{fillOpacity: this.state.fillOpacity}} onClick={this._OnClick}></rect>
-  <line x1={z} y1={w+30} x2={p} y2={w+30} stroke="black"></line>
-  <text x={z+5} y={w+16} stroke="black">{entity}</text>
-  {attributes_map}
+      <g>
+        <rect x={z} y={w} rx="6" ry="6" width="176" height="130.76923076923077" fill="#BFDFBF" stroke="black" style={{fillOpacity: this.state.fillOpacity}} onClick={this._OnClick}></rect>
+        <line x1={z} y1={w+30} x2={p} y2={w+30} stroke="black"></line>
+        <text x={z+5} y={w+16} stroke="black">{entity}</text>
+        {attributes_map}
 	{relationships_map}
 	{rightTable}
-</g>
+      </g>
     );
   }
 };
@@ -115,5 +130,3 @@ class ReadingAttributesD3V4 extends Component {
 ReadingAttributesD3V4.propTypes = {
   section: PropTypes.object
 };
-
-export default ReadingAttributesD3V4;
